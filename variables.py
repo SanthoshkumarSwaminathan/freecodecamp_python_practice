7f5a2b84e3f1b0445c19edc2c07fd1228547f80e
Santhosh Kumar S <santhoshswami107@gmail.com>
2026-03-04 15:37:16 +0530

    Added variables.py python basics practice

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Creating a variables and assigning values to it.
+
+Name = "Santhosh Kumar S"
+Age= 24
+
+print(Name,Age)
+# snake casing 
+
+my_name = "This type of vairable is know as snake case"
+
+# these are comments this is a one line comment 
+
+print(my_name)
