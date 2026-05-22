ac4ebd7f3548346ecd0c0473ab74b9e26a7412bb
Santhosh kumar <santhoshswami107@gmail.com>
2026-05-22 19:15:30 +0530

    Day 1 python revision completed

---
+++ day1_python_revision.py
@@ -0,0 +1,58 @@
+# 1 or -1
+
+my_integer_var = 114
+print("Integer:",my_integer_var)
+
+
+# like -0.3 and 1.2
+
+my_float_var = 5.50
+print('Float', my_float_var)
+
+my_str='santhosh'
+print('String:',my_str)
+
+#boolean True or False
+
+my_boolean_var = False
+print('Boolean:',my_boolean_var) 
+
+# set an unordered collection of unique elements
+my_set_var = {7, 'thala', 5.5}
+print('Set:', my_set_var) 
+print(my_set_var)
+
+# dictionary: A collection of key-value pairs enclosed in curly braces, like
+my_dictionary_var = {'name': 'Santhosh kumar S', 'age':25}
+print('Dictinoary:', my_dictionary_var)
+print(my_dictionary_var)
+
+# tuple an immutable ordered collection of unclosed in parantheses
+my_tuple_var = (1,2,1,"sk",2.3)
+print(my_tuple_var)
+print(my_tuple_var)
+# range a sequence of numbers often used in loops
+
+my_range_var = range(5)
+print("Range",range)
+
+#List an ordered collection of elemets that supports diffrent data types.
+
+my_list = [22, 'Hello world', 3.14, True]
+print(my_list)
+
+# None a special value that represents the absence of a value
+
+my_none_var = None
+print("None:", my_none_var)
+
+my_var1=11.0
+my_var2="hello"
+print(type(my_var1))
+print(type(my_var2))
+
+#isinstance()
+
+isinstance('hello world', str)
+isinstance(123,int)
+print(isinstance(123,int))
